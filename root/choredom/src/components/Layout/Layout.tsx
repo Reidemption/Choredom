cd8518b73cf2ead1394fb3f8e291394bf716c9c5
Reidemption <reidgubler@outlook.com>
2023-02-15 20:39:46 -0700

    fix navbar

--- root/choredom/src/components/Layout/Layout.tsx
+++ root/choredom/src/components/Layout/Layout.tsx
@@ -1,94 +1,222 @@
-import React, { ReactNode, useEffect } from "react";
-// import Navbar from "../Navbar/Navbar";
+import React, { ReactNode } from "react";
 import {
+  IconButton,
   Box,
-  Button,
-  Divider,
+  CloseButton,
   Flex,
-  Spacer,
+  Icon,
+  useColorModeValue,
+  Link,
+  Drawer,
+  DrawerContent,
   Text,
-  VStack,
+  useDisclosure,
+  BoxProps,
+  Spacer,
+  FlexProps,
+  Divider,
 } from "@chakra-ui/react";
-import { useSetRecoilState } from "recoil";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../../firebase/clientApp";
-import { signOut } from "firebase/auth";
-import UserNav from "./UserNav";
-import Link from "next/link";
-interface LayoutProps {
-  children?: ReactNode;
-  // any props that come into the component
+import {
+  FiHome,
+  FiTrendingUp,
+  FiCompass,
+  FiStar,
+  FiSettings,
+  FiMenu,
+} from "react-icons/fi";
+import { IconType } from "react-icons";
+import { ReactText } from "react";
+
+interface LinkItemProps {
+  name: string;
+  icon: IconType;
+}
+const LinkItems: Array<LinkItemProps> = [
+  { name: "Home", icon: FiHome },
+  { name: "Trending", icon: FiTrendingUp },
+  { name: "Explore", icon: FiCompass },
+  { name: "Favourites", icon: FiStar },
+  { name: "Settings", icon: FiSettings },
+];
+
+export default function SimpleSidebar({ children }: { children: ReactNode }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
+      <SidebarContent
+        onClose={() => onClose}
+        display={{ base: "none", md: "block" }}
+      />
+      <Drawer
+        autoFocus={false}
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        returnFocusOnClose={false}
+        onOverlayClick={onClose}
+        size="full"
+      >
+        <DrawerContent>
+          <SidebarContent onClose={onClose} />
+        </DrawerContent>
+      </Drawer>
+      {/* mobilenav */}
+      <MobileNav display={{ base: "flex", md: "none" }} onOpen={onOpen} />
+      <Box ml={{ base: 0, md: 60 }} p="4">
+        {children}
+      </Box>
+    </Box>
+  );
 }
 
-const Layout: React.FC = ({ children }: LayoutProps) => {
-  const [user, loading, error] = useAuthState(auth);
+interface SidebarProps extends BoxProps {
+  onClose: () => void;
+}
 
-  useEffect(() => {
-    if (user) {
-      console.log("User is logged in", user);
-    }
-  }, [user]);
-
+const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
   return (
-    <>
-      {user ? (
-        <Flex height={"100vh"}>
-          <VStack
-            spacing={4}
-            align={"flex-start"}
-            bg={"tomato"}
-            px={"5"}
-            pt={"4"}
-          >
-            <Box>
-              <Text fontSize={"3xl"}>
-                <Link href="/home">Choredom</Link>
-              </Text>
-            </Box>
-            <Box>
-              <Text>
-                <Link href="/feed">Your Chores</Link>
-              </Text>
-            </Box>
-            <Box>
-              <Text>
-                <Link href="/feed/social">Social Feed</Link>
-              </Text>
-            </Box>
-            <Box>
-              <Text>
-                <Link href="/newchore">Add Chore</Link>
-              </Text>
-            </Box>
-
-            <Divider />
-            <Spacer />
-            <Box>
-              <Text>
-                <Link href="/myaccount">My Account</Link>
-              </Text>
-            </Box>
-            <UserNav user={user} />
-            <Divider />
-            <Box pb={"4"}>
-              <Button
-                onClick={() => {
-                  signOut(auth);
-                }}
-              >
-                Log Out
-              </Button>
-            </Box>
-          </VStack>
-          <main>{children}</main>
-        </Flex>
-      ) : (
-        <main>{children}</main>
-      )}
-    </>
+    <Box
+      bg={useColorModeValue("white", "gray.900")}
+      borderRight="1px"
+      borderRightColor={useColorModeValue("gray.200", "gray.700")}
+      w={{ base: "full", md: 60 }}
+      pos="fixed"
+      h="full"
+      {...rest}
+    >
+      <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
+        <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
+          Choredom
+        </Text>
+        <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
+      </Flex>
+      {LinkItems.map((link) => (
+        <NavItem key={link.name} icon={link.icon}>
+          {link.name}
+        </NavItem>
+      ))}
+      <Divider pt={3} w="90%" />
+      <Spacer />
+      <Flex
+        align="center"
+        p="4"
+        mx="4"
+        borderRadius="lg"
+        role="group"
+        cursor="pointer"
+        _hover={{
+          bg: "cyan.400",
+          color: "white",
+        }}
+      >
+        {/* Fill info about the chore locations
+            Check out 'zustand' for state management
+          */}
+        Chore Locations.
+      </Flex>
+      <Spacer />
+      <Flex
+        bottom={4}
+        left={0}
+        position="absolute"
+        width={"90%"}
+        align="center"
+        p="4"
+        mx="3"
+        borderRadius="lg"
+        role="group"
+        cursor="pointer"
+        _hover={{
+          bg: "cyan.400",
+          color: "white",
+        }}
+      >
+        <Link
+          href="#"
+          style={{ textDecoration: "none" }}
+          _focus={{ boxShadow: "none" }}
+        >
+          <Icon
+            mr="4"
+            fontSize="16"
+            _groupHover={{
+              color: "white",
+            }}
+            as={FiSettings}
+          />
+          Admin
+        </Link>
+      </Flex>
+    </Box>
   );
 };
 
-export default Layout;
+interface NavItemProps extends FlexProps {
+  icon: IconType;
+  children: ReactText;
+}
+const NavItem = ({ icon, children, ...rest }: NavItemProps) => {
+  return (
+    <Link
+      href="#"
+      style={{ textDecoration: "none" }}
+      _focus={{ boxShadow: "none" }}
+    >
+      <Flex
+        align="center"
+        p="4"
+        mx="4"
+        borderRadius="lg"
+        role="group"
+        cursor="pointer"
+        _hover={{
+          bg: "cyan.400",
+          color: "white",
+        }}
+        {...rest}
+      >
+        {icon && (
+          <Icon
+            mr="4"
+            fontSize="16"
+            _groupHover={{
+              color: "white",
+            }}
+            as={icon}
+          />
+        )}
+        {children}
+      </Flex>
+    </Link>
+  );
+};
 
-// TODO: Align all content into the center of the page
+interface MobileProps extends FlexProps {
+  onOpen: () => void;
+}
+const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
+  return (
+    <Flex
+      ml={{ base: 0, md: 60 }}
+      px={{ base: 4, md: 24 }}
+      height="20"
+      alignItems="center"
+      bg={useColorModeValue("white", "gray.900")}
+      borderBottomWidth="1px"
+      borderBottomColor={useColorModeValue("gray.200", "gray.700")}
+      justifyContent="flex-start"
+      {...rest}
+    >
+      <IconButton
+        variant="outline"
+        onClick={onOpen}
+        aria-label="open menu"
+        icon={<FiMenu />}
+      />
+
+      <Text fontSize="2xl" ml="8" fontFamily="monospace" fontWeight="bold">
+        Choredom
+      </Text>
+    </Flex>
+  );
+};
