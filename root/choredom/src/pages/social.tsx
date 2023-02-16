c1e565189230d097abfe6ecfaac70e96a74a0140
Reidemption <reidgubler@outlook.com>
2023-02-16 00:33:30 -0700

    change file path

---
+++ root/choredom/src/pages/social.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import SocialTile from "@/src/components/SocialTile";
+import { Box, Center, Grid } from "@chakra-ui/react";
+import SocialTileProps from "@/src/interfaces/SocialTileInterface";
+
+const socialTiles: Array<SocialTileProps> = [
+  {
+    finishedDate: "10/7",
+    choreTitle: "Took over the entire world together :^)",
+    // choreTitle: "Just got married :)",
+    image: "../us.jpg",
+    imageAlt: "The happy couple",
+    choreStory: "We got married in the mountains. It was a beautiful day.",
+  },
+  {
+    finishedDate: "10/7",
+    choreTitle: "Took over the entire world together :^)",
+    // choreTitle: "Just got married :)",
+    image: "../us.jpg",
+    imageAlt: "The happy couple",
+    choreStory: "We got married in the mountains. It was a beautiful day.",
+  },
+  {
+    finishedDate: "10/7",
+    choreTitle: "Took over the entire world together :^)",
+    // choreTitle: "Just got married :)",
+    // image: "../us.jpg",
+    // imageAlt: "The happy couple",
+    choreStory: "We got married in the mountains. It was a beautiful day.",
+  },
+];
+
+interface FeedProps {}
+
+const Feed: React.FC<FeedProps> = () => {
+  return (
+    <Center>
+      <Box>
+        <Grid templateColumns="repeat(2, 1fr)" gap={6} mx={10}>
+          {socialTiles.map((tile) => (
+            <SocialTile key={tile.choreTitle} {...tile} />
+          ))}
+        </Grid>
+      </Box>
+    </Center>
+  );
+};
+
+export default Feed;
