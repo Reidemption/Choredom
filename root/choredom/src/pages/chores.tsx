76ecf1be277f10eb291f35d1039ad777a0063d61
Reidemption <reidgubler@outlook.com>
2023-02-16 00:35:50 -0700

    dynamic chore tile generation and import interface props

--- root/choredom/src/pages/chores.tsx
+++ root/choredom/src/pages/chores.tsx
@@ -1,18 +1,13 @@
 import { Box, Center, Grid, Stack } from "@chakra-ui/react";
 import React from "react";
 import ChoreTile from "../components/ChoreTile";
-
-interface ChoreProps {
-  toDoDate: string;
-  choreName: string;
-  choreDescription?: string;
-  choreFrequency?: string;
-}
+import ChoreProps from "../interfaces/ChoreTileInterface";
 
 const ChoreItems: Array<ChoreProps> = [
   {
     toDoDate: "2/14",
     choreName: "Buy a bouquet of flowers",
+    choreLocation: "Smiths",
     choreDescription:
       "Try going to Smiths or Lins. They typically have good deals on flowers.",
     choreFrequency: "Every year",
@@ -20,10 +15,16 @@ const ChoreItems: Array<ChoreProps> = [
   {
     toDoDate: "2/16",
     choreName: "Laundry",
+    choreLocation: "Laundry room",
     choreDescription: "Colors and darks",
     choreFrequency: "Every week",
   },
-  { toDoDate: "2/15", choreName: "Shopping", choreFrequency: "Every week" },
+  {
+    toDoDate: "2/15",
+    choreName: "Shopping",
+    choreFrequency: "Every week",
+    choreLocation: "Lululemon",
+  },
 ];
 
 const Chore: React.FC = () => {
@@ -31,13 +32,7 @@ const Chore: React.FC = () => {
     <Box>
       <Stack direction={"column"} spacing="6" align={"center"} mt="3">
         {ChoreItems.map((chore) => (
-          <ChoreTile
-            key={chore.choreName}
-            toDoDate={chore.toDoDate}
-            choreName={chore.choreName}
-            choreDescription={chore.choreDescription}
-            choreFrequency={chore.choreFrequency}
-          ></ChoreTile>
+          <ChoreTile key={chore.choreName} {...chore}></ChoreTile>
         ))}
       </Stack>
     </Box>
