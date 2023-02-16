e20b0aeb4efcb01874862db6f8333b8b5184ce5b
Reidemption <reidgubler@outlook.com>
2023-02-15 20:40:19 -0700

    good 1st iteration

--- root/choredom/src/components/ChoreTile.tsx
+++ root/choredom/src/components/ChoreTile.tsx
@@ -1,3 +1,14 @@
+import {
+  Box,
+  Center,
+  Checkbox,
+  Flex,
+  Stack,
+  Spacer,
+  HStack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import React from "react";
 
 interface ChoreTileProps {
@@ -8,7 +19,34 @@ interface ChoreTileProps {
 }
 
 const ChoreTile: React.FC<ChoreTileProps> = (props) => {
-  return <div>Hello World</div>;
+  return (
+    <Flex
+      align="center"
+      justify="center"
+      border={"1px"}
+      borderColor={"blackAlpha.400"}
+      w={500} //Make this responsive
+      bg={"white"}
+    >
+      <Flex w="100%" m={5}>
+        <Checkbox colorScheme="purple" w="10%"></Checkbox>
+        <Spacer />
+        <HStack spacing={3} w="35%">
+          <VStack spacing={0}>
+            <Text>{props.toDoDate}</Text>
+            <Text mt={0}>{props.choreName}</Text>
+            <Text textAlign={"center"} as="i">
+              {props.choreFrequency}
+            </Text>
+          </VStack>
+        </HStack>
+        <Spacer />
+        <Box width={"50%"}>
+          <Text fontSize={"sm"}>{props.choreDescription}</Text>
+        </Box>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default ChoreTile;
