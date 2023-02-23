4a1e8a5631ee91791f448ad80515f62450e568b4
Reidemption <reidgubler@outlook.com>
2023-02-23 15:09:37 -0700

    format changes

--- root/choredom/src/pages/_app.tsx
+++ root/choredom/src/pages/_app.tsx
@@ -1,17 +1,17 @@
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "../chakra/theme";
-import Layout from "../components/Layout/Layout";
-import { RecoilRoot } from "recoil";
+import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../chakra/theme'
+import Layout from '../components/Layout/Layout'
+import { RecoilRoot } from 'recoil'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <RecoilRoot>
-      <ChakraProvider theme={theme}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ChakraProvider>
-    </RecoilRoot>
-  );
+	return (
+		<RecoilRoot>
+			<ChakraProvider theme={theme}>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</ChakraProvider>
+		</RecoilRoot>
+	)
 }
