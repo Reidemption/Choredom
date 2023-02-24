d5f6c1dea73ab32b24b0c7bf6a24485324429781
Reidemption <reidgubler@outlook.com>
2023-02-23 18:03:45 -0700

    loading buffer and remove comments

--- root/choredom/src/pages/chores.tsx
+++ root/choredom/src/pages/chores.tsx
@@ -4,56 +4,44 @@ import ChoreTile from '../components/ChoreTile'
 import ChoreProps from '../interfaces/ChoreTileInterface'
 import { collection, query, where, getDocs } from 'firebase/firestore'
 import { auth, firestore } from '../firebase/clientApp'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 const Chore: React.FC = () => {
 	const [user_chores, setUserChores] = useState<any[]>([])
-	// const [user, setUser] = useState<any>(null)
+	const [loading, setLoading] = useState(true)
 
-	// useEffect(() => {
-	// 	const req = window.indexedDB.open('firebaseLocalStorageDb', 1)
-	// 	req.onsuccess = function (event) {
-	// 		const db = event.target.result
-	// 	}
-	// 	const user = Object.keys(window.indexedDB)
-	// 	// const user = Object.keys(window.indexedDB).filter((item) =>
-	// 	// 	item.startsWith('firebase:authUser')
-	// 	// )[0]
-	// 	console.log('user', user)
-	// 	setUser(user)
-	// }, [])
 	const getChores = async () => {
-		// console.log(user)
+		const auth = getAuth()
+		onAuthStateChanged(auth, async (user) => {
+			try {
+				const choreQuery = query(
+					collection(firestore, 'chores'),
+					where('creatorId', '==', user?.uid)
+				)
+				const choreDocs = await getDocs(choreQuery)
 
-		try {
-			const choreQuery = query(
-				collection(firestore, 'chores'),
-				where('creatorId', '==', 'mUeZerlVKbMuDZ9QvIve72LOqiS2')
-				// TODO: Fix the fact that the user ID is hard-coded
-			)
-			const choreDocs = await getDocs(choreQuery)
-			console.log(choreDocs)
-			const choreData = choreDocs.docs.map((doc) => ({
-				...doc.data(),
-				id: doc.id,
-			}))
-			console.log('choreData:', choreData)
-			setUserChores(choreData)
-		} catch (error) {
-			console.error('getChores error:', error)
-		}
+				const choreData = choreDocs.docs.map((doc) => ({
+					...doc.data(),
+					id: doc.id,
+				}))
+				setUserChores(choreData)
+				setLoading(false)
+			} catch (error) {
+				console.error('getChores error:', error)
+			}
+		})
 	}
 	useEffect(() => {
 		getChores()
 	}, [])
-	// asd
-	// if (!user) {
-	// 	return (
-	// 		<Center>
-	// 			<Text>Not logged in</Text>
-	// 			<Text>Create an account to store chores!</Text>
-	// 		</Center>
-	// 	)
-	// }
+
+	if (loading) {
+		return (
+			<Center>
+				<Text>Loading...</Text>
+			</Center>
+		)
+	}
 
 	return (
 		<Box>
