5a2ca6029c69a0a3545589a58786d8e59c08e928
Reidemption <reidgubler@outlook.com>
2023-02-28 22:00:33 -0700

    chores decided to work again

--- choredom/src/pages/chores.tsx
+++ choredom/src/pages/chores.tsx
@@ -1,4 +1,13 @@
-import { Box, Center, Flex, Grid, Stack, Spinner, Text } from '@chakra-ui/react'
+import {
+	Box,
+	Center,
+	Flex,
+	Grid,
+	Stack,
+	Spinner,
+	Text,
+	Button,
+} from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import ChoreTile from '../components/ChoreTile'
 import ChoreProps from '../interfaces/ChoreTileInterface'
@@ -13,11 +22,15 @@ import {
 } from 'firebase/firestore'
 import { auth, firestore } from '../firebase/clientApp'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { useRecoilState } from 'recoil'
+import { userState } from '../atom/atoms'
+import Link from 'next/link'
 
 const Chore: React.FC = () => {
-	const [currentUser, setCurrentUser] = useState<any>(null)
+	const [currentUser, setCurrentUser] = useRecoilState(userState)
 	const [user_chores, setUserChores] = useState<any[]>([])
-	const [loading, setLoading] = useState(true)
+	const [loading, setLoading] = useState(false)
+	const [oldChores, setOldChores] = useState<boolean>(false)
 
 	const editChore = (id: any) => {
 		console.log('edit chore')
@@ -31,55 +44,98 @@ const Chore: React.FC = () => {
 			isDone: true,
 		})
 		console.log('chore finished')
-		getChoresWithUserAlreadySet()
+		getChores()
 	}
 
-	const getChoresWithUserAlreadySet = async () => {
-		setLoading(true)
+	const getChoresWithGlobalUser = async () => {
 		try {
 			const choreQuery = query(
 				collection(firestore, 'chores'),
-				where('creatorId', '==', currentUser?.uid),
-				// TODO: Fix the orderBy so it works with the date.
-				// - might have to change how date is stored in the database.
-				orderBy('Date')
+				where('creatorId', '==', currentUser?.uid)
 			)
 			const choreDocs = await getDocs(choreQuery)
+			console.log('choreDocs', choreDocs)
 			const choreData = choreDocs.docs.map((doc) => ({
 				...doc.data(),
 				id: doc.id,
 			}))
+			console.log('choreData', choreData)
 			setUserChores(choreData)
-			setLoading(false)
 		} catch (error) {
 			console.error('getChores error:', error)
 		}
 	}
 
-	const getChores = async () => {
+	const getChoresAndSetUser = async () => {
 		const auth = getAuth()
 		onAuthStateChanged(auth, async (user) => {
 			try {
+				const { uid, displayName, email } = user!
+				setCurrentUser({ uid, displayName, email })
 				const choreQuery = query(
 					collection(firestore, 'chores'),
 					where('creatorId', '==', user?.uid)
 				)
 				const choreDocs = await getDocs(choreQuery)
-				setCurrentUser(user)
+				console.log('choreDocs', choreDocs)
+
 				const choreData = choreDocs.docs.map((doc) => ({
 					...doc.data(),
 					id: doc.id,
 				}))
+				console.log(choreData)
 				setUserChores(choreData)
-				setLoading(false)
 			} catch (error) {
 				console.error('getChores error:', error)
 			}
+			setLoading(false)
 		})
 	}
+
+	const getChores = async () => {
+		setLoading(true)
+		console.log('currentUser', currentUser)
+
+		if (!currentUser?.uid) {
+			console.log('no user')
+
+			await getChoresAndSetUser()
+		} else {
+			console.log('user')
+			await getChoresWithGlobalUser()
+		}
+		setLoading(false)
+	}
+
+	const showOldChores = () => {
+		setOldChores(!oldChores)
+	}
+
+	const filteredChores = user_chores.filter((chore) => {
+		return !chore.isDone || oldChores
+	})
+
+	const allChoresAreDone = user_chores.filter((chore) => {
+		return chore.isDone
+	}).length
+
 	useEffect(() => {
-		getChores()
+		async function fetchChores() {
+			await getChores()
+		}
+		fetchChores()
 	}, [])
+
+	if (!currentUser?.uid) {
+		return (
+			<Center h='100vh'>
+				<Text fontSize={'xl'} color={'blue.400'}>
+					<Link href='register'>Create an account&nbsp;</Link>
+				</Text>
+				<Text fontSize='xl'> to keep track of chores!</Text>
+			</Center>
+		)
+	}
 
 	if (loading) {
 		return (
@@ -96,35 +152,35 @@ const Chore: React.FC = () => {
 	}
 	// If the user has no chores
 	// TODO: Add a button to toggle showing completed chores.
-	if (
-		user_chores.filter((chore) => {
-			return !chore.isDone
-		}).length === 0
-	) {
+	if (allChoresAreDone === 0) {
 		return (
 			<Center h='100vh'>
-				<Text fontSize='xl'>You are all caught up on your chores!</Text>
+				<Stack>
+					<Text fontSize='xl'>You are all caught up on your chores!</Text>
+					<Button onClick={showOldChores} colorScheme='purple'>
+						Show Completed Chores
+					</Button>
+				</Stack>
 			</Center>
 		)
 	}
-
 	// TODO: Filter the chores to show them in chronological order.
 	return (
 		<Box>
 			<Stack direction={'column'} spacing='6' align={'center'} mt='3'>
 				{user_chores &&
-					user_chores
-						.filter((chore) => {
-							return !chore.isDone
-						})
-						.map((chore) => (
-							<ChoreTile
-								key={chore.id}
-								props={chore}
-								finishChore={finishChore}
-								editChore={editChore}
-							></ChoreTile>
-						))}
+					filteredChores.map((chore) => (
+						<ChoreTile
+							key={chore.id}
+							props={chore}
+							checked={chore.isDone}
+							finishChore={finishChore}
+							editChore={editChore}
+						></ChoreTile>
+					))}
+				<Button onClick={showOldChores} colorScheme='purple'>
+					Toggle Completed Chores
+				</Button>
 			</Stack>
 		</Box>
 	)
