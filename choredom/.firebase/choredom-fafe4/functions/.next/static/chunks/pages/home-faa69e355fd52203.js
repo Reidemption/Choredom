60d9f2c18fa645a93567c48e7b86ad7b2e7c91d4
Reid Gubler <75095757+Reidemption@users.noreply.github.com>
2023-03-27 11:24:55 -0600

    Delete choredom/.firebase directory

--- choredom/.firebase/choredom-fafe4/functions/.next/static/chunks/pages/home-faa69e355fd52203.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[229],{4309:function(n,e,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/home",function(){return u(1145)}])},1145:function(n,e,u){"use strict";u.r(e);var _=u(5893);u(7294);let t=()=>(0,_.jsx)("div",{children:"Home Page"});e.default=t}},function(n){n.O(0,[774,888,179],function(){return n(n.s=4309)}),_N_E=n.O()}]);
