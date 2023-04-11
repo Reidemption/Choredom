525403a686353e3fb77ac4404a3053cc09561317
Reidemption <75095757+Reidemption@users.noreply.github.com>
2023-04-10 22:18:32 -0600

    UNTESTED: scan for friends then scan for their shared chores

--- choredom/src/pages/social.tsx
+++ choredom/src/pages/social.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SocialTile from '@/src/components/SocialTile'
 import { Box, Center, Grid } from '@chakra-ui/react'
 import SocialTileProps from '@/src/interfaces/SocialTileInterface'
+import { doc, getDoc } from 'firebase/firestore'
+import {auth, firestore} from '@/src/firebase/clientApp'
+import { useRecoilState } from 'recoil'
+import { userState } from '../atom/atoms'
+import { onAuthStateChanged } from 'firebase/auth'
 
 const socialTiles: Array<SocialTileProps> = [
 	{
@@ -33,6 +38,95 @@ const socialTiles: Array<SocialTileProps> = [
 interface FeedProps {}
 
 const Feed: React.FC<FeedProps> = () => {
+	const [gUser, setgUser] = useRecoilState(userState)
+	const [currentUser, setCurrentUser] = React.useState<any>(null)
+	const [friends, setFriends] = useState([])
+	const [shared_chores, setSharedChores] = useState([])
+
+	const getFriends = async () => { 
+		console.log('Getting friends...')
+		try {
+			if (!gUser) {
+				throw new Error('User error. Try again later.')
+			}
+			const friendsRef = doc(firestore, 'friends', gUser.uid)
+			const docSnap = await getDoc(friendsRef)
+			if (!docSnap.exists()) {
+				throw new Error('Possibility of no friends. No document was found.')
+			}
+			let requests = docSnap.data()?.friends
+			setFriends(requests.filter((r: any) => r.status === 'accepted'))
+		} catch (error) {
+			console.log('Error detected in getFriends:', error)
+		}
+	}
+	
+	const getSharedChores = async () => {
+		console.log('Getting friends...')
+		try {
+			if (!gUser) {
+				throw new Error('User error. Try again later.')
+			}
+			console.log('friends:', friends)
+			if (friends.length === 0) {
+				throw new Error('No friends found. Please add friends.')
+			}
+			friends.forEach(async (friend: any) => {
+				const sharedChoresRef = doc(firestore, 'shared', friend.friend_id)
+				const docSnap = await getDoc(sharedChoresRef)
+				if (!docSnap.exists()) {
+					throw new Error('Possibility of no shared chores. No document was found.')
+				}
+				let sharedChores = docSnap.data()
+				console.log('sharedChores', sharedChores)
+				return
+				let temp_array;
+				if (sharedChores) {
+					temp_array = shared_chores.push(sharedChores)
+				}
+				else {
+					temp_array = shared_chores
+				}
+				setSharedChores(temp_array)
+			})
+			const friendsRef = doc(firestore, 'shared', gUser.uid)
+			const docSnap = await getDoc(friendsRef)
+			if (!docSnap.exists()) {
+				throw new Error('Possibility of no friends. No document was found.')
+			}
+			let requests = docSnap.data()?.friends
+			setFriends(requests.filter((r: any) => r.status === 'accepted'))
+		} catch (error) {
+			console.log('Error detected in getFriends:', error)
+		}
+	}
+
+	const checkUserInfo = async () => {
+		 onAuthStateChanged(auth, async (user) => {
+				try {
+					if (!user) {
+						throw new Error('User not found. Please login.')
+					}
+					const { uid, email, displayName } = user!
+					setgUser({ uid, email, displayName })
+					setCurrentUser(user)
+				} catch (error) {
+					console.error(error)
+				}
+			})
+	 }
+
+
+	useEffect(() => {
+		async function onInitialization() {
+			console.log('Feed mounted')
+			checkUserInfo()
+			getFriends()
+			getSharedChores()
+		}
+		onInitialization()
+	}, [])
+
 	return (
 		<Center>
 			<Box>
