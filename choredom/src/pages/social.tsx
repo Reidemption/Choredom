04e27d0563edea60839fbc8939bc227262405e69
Reidemption <reidgubler@outlook.com>
2023-04-12 23:11:19 -0600

    Really just need to correctly await the user and it should be running perfectly

--- choredom/src/pages/social.tsx
+++ choredom/src/pages/social.tsx
@@ -1,70 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import SocialTile from '@/src/components/SocialTile'
-import { Box, Center, Grid } from '@chakra-ui/react'
+import { Box, Center, Grid, Text } from '@chakra-ui/react'
 import SocialTileProps from '@/src/interfaces/SocialTileInterface'
 import { doc, getDoc } from 'firebase/firestore'
 import {auth, firestore} from '@/src/firebase/clientApp'
 import { useRecoilState } from 'recoil'
 import { userState } from '../atom/atoms'
-import { onAuthStateChanged } from 'firebase/auth'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
-const socialTiles: Array<SocialTileProps> = [
-	{
-		finishedDate: '9/30',
-		choreTitle: 'Took over the entire world together :^)',
-		// choreTitle: "Just got married :)",
-		image: '../us.jpg',
-		imageAlt: 'The happy couple',
-		choreStory: 'We got married in the mountains. It was a beautiful day.',
-	},
-	{
-		finishedDate: '9/30',
-		choreTitle: 'Took over the entire world together :^)',
-		// choreTitle: "Just got married :)",
-		image: '../us.jpg',
-		imageAlt: 'The happy couple',
-		choreStory: 'We got married in the mountains. It was a beautiful day.',
-	},
-	{
-		finishedDate: '9/30',
-		choreTitle: 'Took over the entire world together :^)',
-		// choreTitle: "Just got married :)",
-		// image: "../us.jpg",
-		// imageAlt: "The happy couple",
-		choreStory: 'We got married in the mountains. It was a beautiful day.',
-	},
-]
 
-interface FeedProps {}
-
-const Feed: React.FC<FeedProps> = () => {
+const Feed: React.FC<any> = () => {
 	const [gUser, setgUser] = useRecoilState(userState)
-	const [currentUser, setCurrentUser] = React.useState<any>(null)
 	const [friends, setFriends] = useState([])
 	const [shared_chores, setSharedChores] = useState([])
+	const auth = getAuth()
+	const user = auth.currentUser
 
 	const getFriends = async () => { 
 		console.log('Getting friends...')
 		try {
-			if (!gUser) {
+			if (!user) {
 				throw new Error('User error. Try again later.')
 			}
-			const friendsRef = doc(firestore, 'friends', gUser.uid)
+			const friendsRef = doc(firestore, 'friends', user.uid)
 			const docSnap = await getDoc(friendsRef)
 			if (!docSnap.exists()) {
 				throw new Error('Possibility of no friends. No document was found.')
 			}
 			let requests = docSnap.data()?.friends
 			setFriends(requests.filter((r: any) => r.status === 'accepted'))
+			return requests.filter((r: any) => r.status === 'accepted')
 		} catch (error) {
 			console.log('Error detected in getFriends:', error)
 		}
 	}
 	
 	const getSharedChores = async () => {
-		console.log('Getting friends...')
+		console.log('Getting shared chores...')
 		try {
-			if (!gUser) {
+			if (!user) {
 				throw new Error('User error. Try again later.')
 			}
 			console.log('friends:', friends)
@@ -72,7 +46,7 @@ const Feed: React.FC<FeedProps> = () => {
 				throw new Error('No friends found. Please add friends.')
 			}
 			friends.forEach(async (friend: any) => {
-				const sharedChoresRef = doc(firestore, 'shared', friend.friend_id)
+				const sharedChoresRef = doc(firestore, 'sharedChores', friend.friend_id)
 				const docSnap = await getDoc(sharedChoresRef)
 				if (!docSnap.exists()) {
 					throw new Error('Possibility of no shared chores. No document was found.')
@@ -89,7 +63,7 @@ const Feed: React.FC<FeedProps> = () => {
 				}
 				setSharedChores(temp_array)
 			})
-			const friendsRef = doc(firestore, 'shared', gUser.uid)
+			const friendsRef = doc(firestore, 'shared', user.uid)
 			const docSnap = await getDoc(friendsRef)
 			if (!docSnap.exists()) {
 				throw new Error('Possibility of no friends. No document was found.')
@@ -101,43 +75,54 @@ const Feed: React.FC<FeedProps> = () => {
 		}
 	}
 
-	const checkUserInfo = async () => {
-		 onAuthStateChanged(auth, async (user) => {
-				try {
-					if (!user) {
-						throw new Error('User not found. Please login.')
-					}
-					const { uid, email, displayName } = user!
-					setgUser({ uid, email, displayName })
-					setCurrentUser(user)
-				} catch (error) {
-					console.error(error)
-				}
-			})
-	 }
+	// const checkUserInfo = async () => {
+	// 	 onAuthStateChanged(auth, async (user) => {
+	// 			try {
+	// 				if (!user) {
+	// 					throw new Error('User not found. Please login.')
+	// 				}
+	// 				console.log(user);
+	// 				const { uid, email, displayName } = user!
+	// 				setgUser({ uid, email, displayName })
+	// 				return uid
+	// 			} catch (error) {
+	// 				console.error(error)
+	// 			}
+	// 	 })
+	//  }
 
 
 	useEffect(() => {
-		async function onInitialization() {
-			console.log('Feed mounted')
-			checkUserInfo()
-			getFriends()
-			getSharedChores()
+		async function onLoad() {
+			console.log('onLoad');
+			console.log('user', user);
+			await getFriends()
+			await getSharedChores()
 		}
-		onInitialization()
+		
+		onLoad()
 	}, [])
 
-	return (
-		<Center>
-			<Box>
-				<Grid templateColumns='repeat(, 1fr)' gap={6}>
-					{socialTiles.map((tile) => (
-						<SocialTile key={tile.choreTitle} {...tile} />
-					))}
-				</Grid>
-			</Box>
-		</Center>
-	)
+	if (shared_chores.length === 0) {
+		return (
+			<Center h='100vh'>
+				<Text>
+					No friends have shared a chore 😭
+				</Text>
+			</Center>
+		)
+	}
+		return (
+			<Center>
+				<Box>
+					<Grid templateColumns='repeat(, 1fr)' gap={6}>
+						{shared_chores.map((tile) => (
+							<SocialTile key={tile.choreTitle} {...tile} />
+						))}
+					</Grid>
+				</Box>
+			</Center>
+		)
 }
 
 export default Feed
